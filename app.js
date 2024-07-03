27e78ca28a6bb892d4281ea262113e68caca6aa4
Nawwaf Syauqi Achmad <nawaf.syauqi.10@gmail.com>
2024-07-03 16:18:49 +0700

    element front-end

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
